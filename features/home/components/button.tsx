9a4720f5b93501b07de24db7c114e42a383efa54
Rashid Shaikh <contactrashid8@gamil.com>
2026-05-01 19:11:10 +0530

    Make button reusable

--- features/home/components/button.tsx
+++ features/home/components/button.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { ActivityIndicator, StyleSheet, Text, TouchableOpacity } from "react-native";
 
-type FetchButtonProps = {
+type ButtonProps = {
   isLoading: boolean;
   onPress: () => void;
+  label: string;
+  style?: Object;
 };
 
-export default function FetchButton({ isLoading, onPress }: FetchButtonProps) {
+export default function Button({ isLoading, onPress, label, style }: ButtonProps) {
   return (
     <TouchableOpacity
       accessibilityRole="button"
       activeOpacity={0.88}
       disabled={isLoading}
       onPress={onPress}
-      style={[styles.button, isLoading && styles.buttonDisabled]}
+      style={[ styles.button, isLoading && styles.buttonDisabled , style]}
     >
-      {isLoading ? <ActivityIndicator color="#ffffff" /> : <Text style={styles.label}>Fetch Data</Text>}
+      {isLoading ? <ActivityIndicator color="#ffffff" /> : <Text style={styles.label}>{label}</Text>}
     </TouchableOpacity>
   );
 }
